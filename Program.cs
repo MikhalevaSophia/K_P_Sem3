e7dc1e66adfe55d51c74a28b1687af9537125fcf
Sophia <112760887+MikhalevaSophia@users.noreply.github.com>
2023-12-13 10:48:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+
+        // Создание и запуск главной формы
+        Application.Run(new MainForm());
+    }
+}
+
+public class MainForm : Form
+{
+    private TextBox outputTextBox;
+    private Button runButton;
+    private OpenFileDialog openFileDialog;
+
+    public MainForm()
+    {
+        // Инициализация компонентов формы
+        InitializeComponents();
+
+        // Подготовка файла для записи журнала событий
+        PrepareEventLogFile();
+    }
+
+    private void InitializeComponents()
+    {
+        outputTextBox = new TextBox
+        {
+            Multiline = true,
+            ScrollBars = ScrollBars.Vertical,
+            Dock = DockStyle.Fill,
+            ReadOnly = true
+        };
+
+        runButton = new Button
+        {
+            Text = "Запустить",
+            Dock = DockStyle.Bottom
+        };
+
+        runButton.Click += RunButton_Click;
+
+        openFileDialog = new OpenFileDialog
+        {
+            Title = "Выберите файл t_help.txt",
+            Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*"
+        };
+
+        Controls.Add(outputTextBox);
+        Controls.Add(runButton);
+
+        Text = "Утилита PowerShell";
+        Size = new System.Drawing.Size(400, 300);
+    }
+
+    private void RunButton_Click(object sender, EventArgs e)
+    {
+        if (openFileDialog.ShowDialog() == DialogResult.OK)
+        {
+            // Получение выбранного файла
+            string selectedFilePath = openFileDialog.FileName;
+
+            // Вызов метода для выполнения скрипта PowerShell
+            RunPowerShellScript(selectedFilePath);
+
+            // Обновление содержимого файла event.txt
+            UpdateEventLogFile();
+        }
+    }
+
+    private void RunPowerShellScript(string scriptFilePath)
+    {
+        try
+        {
+            // Чтение содержимого файла
+            string scriptContent = File.ReadAllText(scriptFilePath);
+
+            // Создание процесса PowerShell
+            using (Process PowerShellProcess = new Process())
+            {
+                // Настройка параметров процесса PowerShell
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = "t_help.ps1",
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                PowerShellProcess.StartInfo = startInfo;
+
+                // Запуск процесса PowerShell
+                PowerShellProcess.Start();
+
+                // Передача скрипта в стандартный ввод PowerShell
+                StreamWriter sw = PowerShellProcess.StandardInput;
+                sw.WriteLine(scriptContent);
+                sw.Close();
+
+                // Чтение вывода и ошибок
+                string output = PowerShellProcess.StandardOutput.ReadToEnd();
+                string error = PowerShellProcess.StandardError.ReadToEnd();
+
+                // Вывод результатов в текстовое поле
+                outputTextBox.Text = "Вывод скрипта PowerShell:\r\n" + output;
+
+                if (!string.IsNullOrEmpty(error))
+                {
+                    outputTextBox.Text += "\r\nОшибка выполнения скрипта PowerShell:\r\n" + error;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            outputTextBox.Text = "Произошла ошибка: " + ex.Message;
+        }
+    }
+
+    private void PrepareEventLogFile()
+    {
+        // Путь к файлу событий
+        string eventLogFilePath = "event.txt";
+
+        // Создание/пересоздание файла
+        File.WriteAllText(eventLogFilePath, "");
+    }
+
+    private void UpdateEventLogFile()
+    {
+        // Путь к файлу событий
+        string eventLogFilePath = "event.txt";
+
+        try
+        {
+            // Запись в файл
+            File.AppendAllText(eventLogFilePath, DateTime.Now.ToString() + ": Запуск скрипта PowerShell\r\n");
+        }
+        catch (Exception ex)
+        {
+            outputTextBox.Text += "\r\nОшибка записи в файл событий:\r\n" + ex.Message;
+        }
+    }
+}
---
+++ t_help.ps1
@@ -0,0 +1,41 @@
+﻿# Установка модуля Git-WinEvent (если не установлен)
+if (-not (Get-Module -Name Git-WinEvent -ListAvailable)) {
+    Install-Module -Name Git-WinEvent -Force -Scope CurrentUser
+}
+
+# Функция для скачивания журнала событий и записи в файл
+function DownloadEventLog {
+    param(
+        [string]$vmIpAddress,
+        [string]$sshUsername,
+        [string]$sshKeyPath,
+        [string]$sshPassword
+    )
+
+    # Путь к файлу, в который будет записан журнал событий
+    $eventLogFilePath = "путь_к_event.txt"
+
+    # Формирование команды PowerShell для скачивания журнала событий
+    $powershellCommand = "Get-WinEvent -LogName 'System', 'Application' | Out-File -FilePath $eventLogFilePath"
+
+    # Формирование команды SSH для выполнения команды PowerShell
+    $sshCommandWithPowerShell = "ssh -i $sshKeyPath $sshUsername@$vmIpAddress '$powershellCommand'"
+
+    try {
+        # Попытка скачивания журнала событий на виртуальной машине
+        Invoke-Expression -Command $sshCommandWithPowerShell
+        Write-Host "Журнал событий успешно скачан и сохранен в $eventLogFilePath"
+    } catch {
+        Write-Host "Не удалось скачать журнал событий."
+        Write-Host "Ошибка: $_"
+    }
+}
+
+# Параметры для подключения
+$vmIpAddress = "IP_вашей_виртуальной_машины"
+$sshUsername = "ваш_пользователь"
+$sshKeyPath = "путь_к_вашему_ключу"
+$sshPassword = "ваш_пароль"
+
+# Вызов функции для скачивания журнала событий из виртуальной машины
+DownloadEventLog -vmIpAddress $vmIpAddress -sshUsername $sshUsername -sshKeyPath $sshKeyPath -sshPassword $sshPassword
